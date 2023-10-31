754961c0eb9b5c5fbff6ea113ddad1a49d3de7a5
RefadliDWiIlham <149132853+RefadliDWiIlham@users.noreply.github.com>
2023-10-31 11:48:14 +0700

    Add files via upload

---
+++ estimasi_ponsel.py
@@ -0,0 +1,24 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_harga_ponsel.sav','rb'))
+
+st.title('ESTIMASI HARGA MOBIL MERC')
+
+Sale = st.number_input('MASUKAN JUMLAH PENJUALAN')
+weight = st.number_input('MASUKAN BERAT')
+ppi = st.number_input('MASUKAN PPI')
+cpu_core = st.number_input('MASUKAN CPU CORE')
+ram = st.number_input('MASUKAN RAM')
+Front_Cam =st.number_input('MASUKAN FRONT CAM')
+battery =st.number_input('MASUKAN BATTERY')
+thickness =st.number_input('MASUKAN KETEBALAN')
+
+predict = ''
+
+if st.button('ESTIMASI'):
+    predict = model.predict(
+        [[Sale,weight,ppi,cpu_core,ram,Front_Cam,battery,thickness]]
+    )
+
+    st.write ('ESTIMASI HARGA PONSEL:', predict)
